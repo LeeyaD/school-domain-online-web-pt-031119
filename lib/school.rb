ea54b9f7fa97ed352fde299dd9315fec452572b9
Leeya Davis <leeya463@gmail.com>
2019-03-30 22:38:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -20,14 +20,14 @@ class School
     @roster[grade]
   end
 
-  def sort
-    sorted_roster = {}
-    @roster.each do |grade, names|
-      sorted_roster[grade] = names.sort
-    end
+  #def sort
+    #sorted_roster = {}
+    #@roster.each do |grade, names|
+    #  sorted_roster[grade] = names.sort
+    #end
 
-    sorted_roster
+    #sorted_roster
 
-  end
+  #end
 
 end
